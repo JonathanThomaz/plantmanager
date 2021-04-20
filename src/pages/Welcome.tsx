a90415226252046d3b3d0e0cfa60b018ae2a2a00
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2021-04-20 20:56:23 -0300

    refactor: adjust button

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import warteringImg from '../assets/watering.png';
-import {Button }from '../components/Button';
 import colors from '../styles/colors';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-around'
     },
     title: {
         fontSize: 32,
@@ -27,16 +26,39 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: colors.body_dark
     },
+    image: {
+        height: Dimensions.get('window').width * 0.7
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56,
+    },
+    buttonTitle: {
+        color: colors.white,
+        fontSize: 24,
+    }
 })
 
 export function Welcome() {
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Gerencie {'\n'}suas plantas de {'\n'}forma fácil</Text>
-            <Image source={warteringImg} />
+            <Image source={warteringImg} style={styles.image} resizeMode="contain"/>
             <Text style={styles.subTitle}> Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
                 sempre que precisar.</Text>
-            <Button title='>'/>
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.6}
+            >
+                <Text style={styles.buttonTitle}>
+                    {'>'}
+                </Text>
+            </TouchableOpacity>
 
         </SafeAreaView>
     )
