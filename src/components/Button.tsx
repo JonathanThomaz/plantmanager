b4597e1517dc6d8a6247f1da6797b784da9df9c0
Jonathan Thomaz <jonathanthomaz96@gmail.com>
2021-04-21 01:43:58 -0300

    fix: fix button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps {
     title: string;
@@ -14,18 +15,19 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         marginBottom: 10,
         height: 56,
-        width: 56,
+        width: 231,
     },
     buttonTitle: {
         color: colors.white,
         fontSize: 24,
+        fontFamily: fonts.complement,
     }
 })
 export function Button({ title, ...rest }: ButtonProps) {
     return (
         <TouchableOpacity
             style={styles.button}
-            activeOpacity={0.6}
+            activeOpacity={0.5}
             {...rest}
         >
             <Text style={styles.buttonTitle}>
